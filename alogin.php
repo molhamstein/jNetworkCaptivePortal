4b6ee6e7b93f9ad7b33614029c60c3376a895a6b
Molham Mahmoud <fatherboard1@gmail.com>
2019-01-07 20:55:48 +0200

    make alogin button hidden by default

--- alogin.php
+++ alogin.php
@@ -84,7 +84,7 @@
     <svg width="65px"  height="65px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background: none;"><circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#ffffff" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(282 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>
   </div>
 </div>
-<div id="link-btn-div" class="pulse-btn ">
+<div id="link-btn-div" class="pulse-btn hidden">
   <div class="pulse-btn-bg"></div>
    <div class="pulse-btn-btn"><a id="link-btn" href="" target="_blank"><i class="fa fa-chevron-down" aria-hidden="true"></i></a></button>
 </div>
